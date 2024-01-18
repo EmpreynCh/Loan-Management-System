94d9ba4266bb5ee7882ba70520ad885524565c5e
EmpreynCh <emprhaimquiambao03@gmail.com>
2024-01-18 21:42:46 +0800

    vbnet project

---
+++ LoanManagementSystem/Loan/Form1.Designer.vb
@@ -0,0 +1,516 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TXTLRN = New TextBox()
+        Label2 = New Label()
+        TXTACCNUM = New TextBox()
+        TXTNAME = New TextBox()
+        TXTADD = New TextBox()
+        TXTALOAN = New TextBox()
+        TXTYTPAY = New TextBox()
+        TXTINRATE = New TextBox()
+        TXTLRN2 = New TextBox()
+        TXTACCNUM2 = New TextBox()
+        TXTMONPAY = New TextBox()
+        TXTNAME2 = New TextBox()
+        TXTADD2 = New TextBox()
+        TXTALOAN2 = New TextBox()
+        TXTINRATE2 = New TextBox()
+        TXTYTPAY2 = New TextBox()
+        TOTALIN = New TextBox()
+        TOTALANNUALPAYMENT = New TextBox()
+        TOTALLOANPAYMENT = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        BTNCLEAR = New Button()
+        BTNCOM = New Button()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Label17 = New Label()
+        Label18 = New Label()
+        Label19 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ActiveCaptionText
+        Label1.Location = New Point(231, 27)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(480, 50)
+        Label1.TabIndex = 0
+        Label1.Text = "Loan Management System"
+        ' 
+        ' TXTLRN
+        ' 
+        TXTLRN.Location = New Point(193, 129)
+        TXTLRN.Name = "TXTLRN"
+        TXTLRN.Size = New Size(218, 27)
+        TXTLRN.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = SystemColors.ActiveCaptionText
+        Label2.Location = New Point(54, 129)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(133, 20)
+        Label2.TabIndex = 2
+        Label2.Text = "Loan Ref Number"
+        ' 
+        ' TXTACCNUM
+        ' 
+        TXTACCNUM.Location = New Point(193, 173)
+        TXTACCNUM.Name = "TXTACCNUM"
+        TXTACCNUM.Size = New Size(218, 27)
+        TXTACCNUM.TabIndex = 1
+        ' 
+        ' TXTNAME
+        ' 
+        TXTNAME.Location = New Point(193, 221)
+        TXTNAME.Name = "TXTNAME"
+        TXTNAME.Size = New Size(218, 27)
+        TXTNAME.TabIndex = 1
+        ' 
+        ' TXTADD
+        ' 
+        TXTADD.Location = New Point(193, 266)
+        TXTADD.Name = "TXTADD"
+        TXTADD.Size = New Size(218, 27)
+        TXTADD.TabIndex = 1
+        ' 
+        ' TXTALOAN
+        ' 
+        TXTALOAN.Location = New Point(193, 314)
+        TXTALOAN.Name = "TXTALOAN"
+        TXTALOAN.Size = New Size(218, 27)
+        TXTALOAN.TabIndex = 1
+        ' 
+        ' TXTYTPAY
+        ' 
+        TXTYTPAY.Location = New Point(193, 409)
+        TXTYTPAY.Name = "TXTYTPAY"
+        TXTYTPAY.Size = New Size(218, 27)
+        TXTYTPAY.TabIndex = 1
+        ' 
+        ' TXTINRATE
+        ' 
+        TXTINRATE.Location = New Point(193, 362)
+        TXTINRATE.Name = "TXTINRATE"
+        TXTINRATE.Size = New Size(218, 27)
+        TXTINRATE.TabIndex = 1
+        ' 
+        ' TXTLRN2
+        ' 
+        TXTLRN2.Location = New Point(644, 122)
+        TXTLRN2.Name = "TXTLRN2"
+        TXTLRN2.Size = New Size(218, 27)
+        TXTLRN2.TabIndex = 1
+        ' 
+        ' TXTACCNUM2
+        ' 
+        TXTACCNUM2.Location = New Point(644, 173)
+        TXTACCNUM2.Name = "TXTACCNUM2"
+        TXTACCNUM2.Size = New Size(218, 27)
+        TXTACCNUM2.TabIndex = 1
+        ' 
+        ' TXTMONPAY
+        ' 
+        TXTMONPAY.Location = New Point(353, 479)
+        TXTMONPAY.Name = "TXTMONPAY"
+        TXTMONPAY.Size = New Size(218, 27)
+        TXTMONPAY.TabIndex = 1
+        ' 
+        ' TXTNAME2
+        ' 
+        TXTNAME2.Location = New Point(644, 221)
+        TXTNAME2.Name = "TXTNAME2"
+        TXTNAME2.Size = New Size(218, 27)
+        TXTNAME2.TabIndex = 1
+        ' 
+        ' TXTADD2
+        ' 
+        TXTADD2.Location = New Point(644, 263)
+        TXTADD2.Name = "TXTADD2"
+        TXTADD2.Size = New Size(218, 27)
+        TXTADD2.TabIndex = 1
+        ' 
+        ' TXTALOAN2
+        ' 
+        TXTALOAN2.Location = New Point(644, 310)
+        TXTALOAN2.Name = "TXTALOAN2"
+        TXTALOAN2.Size = New Size(218, 27)
+        TXTALOAN2.TabIndex = 1
+        ' 
+        ' TXTINRATE2
+        ' 
+        TXTINRATE2.Location = New Point(644, 362)
+        TXTINRATE2.Name = "TXTINRATE2"
+        TXTINRATE2.Size = New Size(218, 27)
+        TXTINRATE2.TabIndex = 1
+        ' 
+        ' TXTYTPAY2
+        ' 
+        TXTYTPAY2.Location = New Point(644, 409)
+        TXTYTPAY2.Name = "TXTYTPAY2"
+        TXTYTPAY2.Size = New Size(218, 27)
+        TXTYTPAY2.TabIndex = 1
+        ' 
+        ' TOTALIN
+        ' 
+        TOTALIN.Location = New Point(353, 524)
+        TOTALIN.Name = "TOTALIN"
+        TOTALIN.Size = New Size(218, 27)
+        TOTALIN.TabIndex = 1
+        ' 
+        ' TOTALANNUALPAYMENT
+        ' 
+        TOTALANNUALPAYMENT.Location = New Point(353, 570)
+        TOTALANNUALPAYMENT.Name = "TOTALANNUALPAYMENT"
+        TOTALANNUALPAYMENT.Size = New Size(218, 27)
+        TOTALANNUALPAYMENT.TabIndex = 1
+        ' 
+        ' TOTALLOANPAYMENT
+        ' 
+        TOTALLOANPAYMENT.Location = New Point(353, 618)
+        TOTALLOANPAYMENT.Name = "TOTALLOANPAYMENT"
+        TOTALLOANPAYMENT.Size = New Size(218, 27)
+        TOTALLOANPAYMENT.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.ForeColor = SystemColors.ActiveCaptionText
+        Label3.Location = New Point(54, 173)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(129, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Account Number"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.ForeColor = SystemColors.ActiveCaptionText
+        Label4.Location = New Point(62, 221)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(51, 20)
+        Label4.TabIndex = 2
+        Label4.Text = "Name"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.ForeColor = SystemColors.ActiveCaptionText
+        Label5.Location = New Point(62, 269)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(66, 20)
+        Label5.TabIndex = 2
+        Label5.Text = "Address"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.ForeColor = SystemColors.ActiveCaptionText
+        Label6.Location = New Point(62, 317)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(105, 20)
+        Label6.TabIndex = 2
+        Label6.Text = "Amount Loan"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.ForeColor = SystemColors.ActiveCaptionText
+        Label7.Location = New Point(62, 365)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(100, 20)
+        Label7.TabIndex = 2
+        Label7.Text = "Interest Rate"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.ForeColor = SystemColors.ActiveCaptionText
+        Label8.Location = New Point(62, 412)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(94, 20)
+        Label8.TabIndex = 2
+        Label8.Text = "Years to Pay"
+        ' 
+        ' BTNCLEAR
+        ' 
+        BTNCLEAR.BackColor = Color.Lavender
+        BTNCLEAR.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        BTNCLEAR.ForeColor = SystemColors.ActiveCaptionText
+        BTNCLEAR.Location = New Point(670, 473)
+        BTNCLEAR.Name = "BTNCLEAR"
+        BTNCLEAR.Size = New Size(157, 59)
+        BTNCLEAR.TabIndex = 3
+        BTNCLEAR.Text = "CLEAR"
+        BTNCLEAR.UseVisualStyleBackColor = False
+        ' 
+        ' BTNCOM
+        ' 
+        BTNCOM.BackColor = Color.Lavender
+        BTNCOM.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        BTNCOM.ForeColor = SystemColors.ActiveCaptionText
+        BTNCOM.Location = New Point(670, 570)
+        BTNCOM.Name = "BTNCOM"
+        BTNCOM.Size = New Size(157, 59)
+        BTNCOM.TabIndex = 3
+        BTNCOM.Text = "COMPUTE"
+        BTNCOM.UseVisualStyleBackColor = False
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label9.ForeColor = SystemColors.ActiveCaptionText
+        Label9.Location = New Point(498, 122)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(133, 20)
+        Label9.TabIndex = 2
+        Label9.Text = "Loan Ref Number"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label10.ForeColor = SystemColors.ActiveCaptionText
+        Label10.Location = New Point(502, 173)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(129, 20)
+        Label10.TabIndex = 2
+        Label10.Text = "Account Number"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label11.ForeColor = SystemColors.ActiveCaptionText
+        Label11.Location = New Point(502, 221)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(51, 20)
+        Label11.TabIndex = 2
+        Label11.Text = "Name"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label12.ForeColor = SystemColors.ActiveCaptionText
+        Label12.Location = New Point(502, 263)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(66, 20)
+        Label12.TabIndex = 2
+        Label12.Text = "Address"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label13.ForeColor = SystemColors.ActiveCaptionText
+        Label13.Location = New Point(502, 362)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(100, 20)
+        Label13.TabIndex = 2
+        Label13.Text = "Interest Rate"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label14.ForeColor = SystemColors.ActiveCaptionText
+        Label14.Location = New Point(502, 310)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(105, 20)
+        Label14.TabIndex = 2
+        Label14.Text = "Amount Loan"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label15.ForeColor = SystemColors.ActiveCaptionText
+        Label15.Location = New Point(502, 409)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(94, 20)
+        Label15.TabIndex = 2
+        Label15.Text = "Years to Pay"
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label16.ForeColor = SystemColors.ActiveCaptionText
+        Label16.Location = New Point(207, 479)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(138, 20)
+        Label16.TabIndex = 2
+        Label16.Text = "Monthly Payment:"
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label17.ForeColor = SystemColors.ActiveCaptionText
+        Label17.Location = New Point(242, 524)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(103, 20)
+        Label17.TabIndex = 2
+        Label17.Text = "Total Interest"
+        ' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label18.ForeColor = SystemColors.ActiveCaptionText
+        Label18.Location = New Point(183, 570)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(164, 20)
+        Label18.TabIndex = 2
+        Label18.Text = "Total Annual Payment"
+        ' 
+        ' Label19
+        ' 
+        Label19.AutoSize = True
+        Label19.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label19.ForeColor = SystemColors.ActiveCaptionText
+        Label19.Location = New Point(199, 618)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(148, 20)
+        Label19.TabIndex = 2
+        Label19.Text = "Total Loan Payment"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
+        ClientSize = New Size(989, 739)
+        Controls.Add(BTNCOM)
+        Controls.Add(BTNCLEAR)
+        Controls.Add(Label14)
+        Controls.Add(Label19)
+        Controls.Add(Label18)
+        Controls.Add(Label17)
+        Controls.Add(Label16)
+        Controls.Add(Label15)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(TXTINRATE2)
+        Controls.Add(TXTINRATE)
+        Controls.Add(TXTYTPAY2)
+        Controls.Add(TXTYTPAY)
+        Controls.Add(TXTALOAN2)
+        Controls.Add(TXTALOAN)
+        Controls.Add(TXTADD2)
+        Controls.Add(TXTADD)
+        Controls.Add(TXTNAME2)
+        Controls.Add(TXTNAME)
+        Controls.Add(TOTALLOANPAYMENT)
+        Controls.Add(TOTALANNUALPAYMENT)
+        Controls.Add(TOTALIN)
+        Controls.Add(TXTMONPAY)
+        Controls.Add(TXTACCNUM2)
+        Controls.Add(TXTACCNUM)
+        Controls.Add(TXTLRN2)
+        Controls.Add(TXTLRN)
+        Controls.Add(Label1)
+        ForeColor = SystemColors.ControlDarkDark
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TXTLRN As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TXTACCNUM As TextBox
+    Friend WithEvents TXTNAME As TextBox
+    Friend WithEvents TXTADD As TextBox
+    Friend WithEvents TXTALOAN As TextBox
+    Friend WithEvents TXTYTPAY As TextBox
+    Friend WithEvents TXTINRATE As TextBox
+    Friend WithEvents TXTLRN2 As TextBox
+    Friend WithEvents TXTACCNUM2 As TextBox
+    Friend WithEvents TXTMONPAY As TextBox
+    Friend WithEvents TXTNAME2 As TextBox
+    Friend WithEvents TXTADD2 As TextBox
+    Friend WithEvents TXTALOAN2 As TextBox
+    Friend WithEvents TXTINRATE2 As TextBox
+    Friend WithEvents TXTYTPAY2 As TextBox
+    Friend WithEvents TOTALIN As TextBox
+    Friend WithEvents TOTALANNUALPAYMENT As TextBox
+    Friend WithEvents TOTALLOANPAYMENT As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents BTNCLEAR As Button
+    Friend WithEvents BTNCOM As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+End Class
---
+++ LoanManagementSystem/Loan/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Security.Principal
+
+Public Class Form1
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TXTALOAN.TextChanged
+
+    End Sub
+
+    Private Sub TextBox8_TextChanged(sender As Object, e As EventArgs) Handles TXTLRN2.TextChanged
+
+    End Sub
+    Dim code As Integer = 1
+    Private Sub BTNCLEAR_Click(sender As Object, e As EventArgs) Handles BTNCLEAR.Click
+        code += 1
+        TXTLRN.Text = code.ToString("0000")
+        TXTACCNUM.Text = " "
+        TXTNAME.Text = " "
+        TXTADD.Text = " "
+        TXTALOAN.Text = " "
+        TXTINRATE.Text = " "
+        TXTYTPAY.Text = " "
+        TXTLRN2.Text = code.ToString("0000")
+        TXTACCNUM2.Text = " "
+        TXTNAME2.Text = " "
+        TXTADD2.Text = " "
+        TXTALOAN2.Text = " "
+        TXTINRATE2.Text = " "
+        TXTYTPAY2.Text = " "
+        TXTMONPAY.Text = "0.00"
+        TOTALIN.Text = "0.00"
+        TOTALANNUALPAYMENT.Text = "0.00"
+        TOTALLOANPAYMENT.Text = "0.00"
+    End Sub
+
+    Private Sub BTNCOM_Click(sender As Object, e As EventArgs) Handles BTNCOM.Click
+        Try
+            Dim ACCNUM As Integer = TXTACCNUM.Text
+            Dim NAME As String = TXTNAME.Text
+            Dim ADD As String = TXTADD.Text
+            Dim AMLOAN As Integer = TXTALOAN.Text
+            Dim INT As Double = TXTINRATE.Text
+            Dim YEARS As Integer = TXTYTPAY.Text
+            TXTACCNUM2.Text = ACCNUM
+            TXTNAME2.Text = NAME
+            TXTADD2.Text = ADD
+            TXTALOAN2.Text = AMLOAN
+            TXTINRATE2.Text = +INT
+            TXTYTPAY2.Text = YEARS
+            Dim RATE = INT / 100
+            Dim TIME = YEARS * 12
+            Dim TOTIN = (AMLOAN * RATE) * YEARS
+            Dim TOTLOAN = AMLOAN + TOTIN
+            Dim TNP = TOTLOAN / YEARS
+            Dim MP = TNP / 12
+
+            TXTMONPAY.Text = Format(MP, "₱#,###.00")
+            TOTALIN.Text = Format(TOTIN, "₱#,###.00")
+            TOTALLOANPAYMENT.Text = Format(TNP, "₱#,###.00")
+            TOTALLOANPAYMENT.Text = Format(TOTLOAN, "₱#,###.00")
+        Catch ex As Exception
+            MsgBox("Input a valid number!")
+        End Try
+    End Sub
+
+    Private Sub TOTALIN_TextChanged(sender As Object, e As EventArgs) Handles TOTALIN.TextChanged
+
+    End Sub
+End Class
